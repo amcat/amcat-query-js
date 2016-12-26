85df6ec44d321f2cd0a0379336db6cdbec3efb24
Martijn Bastiaan <martijn@hmbastiaan.nl>
2016-12-26 15:39:18 +0100

    Pass project to search API in articlemodal

--- query/utils/articlemodal.js
+++ query/utils/articlemodal.js
@@ -132,10 +132,13 @@ define([
         function getSearchFilters(filters){
             var new_filters = {};
 
+            console.log(filters);
+
             var field_map = {
                 mediums: "mediumid", query: "q", article_ids: "ids",
                 start_date: "start_date", end_date: "end_date",
-                articlesets: "sets", sets: "sets", on_date: "on_date"
+                articlesets: "sets", sets: "sets", on_date: "on_date",
+                project: "project"
             };
 
             var value_map = {
