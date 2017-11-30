412b5fe3e4c9a89f8fe041913c3d53c5487a91eb
Christian Stuart <christian.jac.stuart@gmail.com>
2017-12-01 00:06:32 +0100

    better date renderers

--- query/valuerenderers.js
+++ query/valuerenderers.js
@@ -1,20 +1,46 @@
 "use strict";
 define(["moment"], function(moment){
 
+    class RenderFnFactory{
+        getRenderFn(match){
+            // not implemented
+        }
+    }
+
     class Renderer{
         constructor(pattern, renderfn){
             this.pattern = pattern;
             this.renderfn = renderfn;
         }
 
-        matches(name){
-            return name.match(this.pattern) !== null;
+        getRenderFn(match){
+            if(this.renderfn instanceof RenderFnFactory){
+                return this.renderfn.getRenderFn(match);
+            }
+            return this.renderfn;
+        }
+
+        match(name){
+            return name.match(this.pattern);
         }
     }
 
-    function renderDate(dateStr){
-        return moment(dateStr).format("DD-MM-YYYY");
+    const date_fmts = {
+        "day": "YYYY-MM-DD",
+        "week": "YYYY-[W]WW",
+        "month": "YYYY-MM",
+        "quarter": "YYYY-[Q]Q",
+        "year": "YYYY",
+    };
+
+    class DateRenderFnFactory extends RenderFnFactory {
+        getRenderFn(match){
+            let fmt = date_fmts[match[1]];
+            fmt = fmt === undefined ? "YYYY-MM-DD" : fmt;
+            return (str => moment(str).format(fmt));
+        }
     }
+
     function renderIdAndName(obj){
         return obj.id + " - " + obj.label;
     }
@@ -27,17 +53,20 @@ define(["moment"], function(moment){
         new Renderer(/^schemafield$/, renderIdAndName),
         new Renderer(/^term$/, term => term.id),
         new Renderer(/^total$/, _ => 'Total'),
-        new Renderer(/^date_(day|week|month|quarter|year)$/, renderDate),
+        new Renderer(/^date_(day|week|month|quarter|year)$/, new DateRenderFnFactory()),
         new Renderer(/_(str|int|num|url|id|tag)$/, renderAsString),
-        new Renderer(/_date$/, renderDate)
+        new Renderer(/_date$/, new DateRenderFnFactory())
     ];
 
     function getRenderer(name, deflt){
         for(let renderer of renderers){
-            if(!renderer.matches(name)){
+            let match = renderer.match(name);
+            if(match === null){
                 continue;
             }
-            return renderer.renderfn;
+            const fn = renderer.getRenderFn(match);
+            console.log(fn);
+            return fn;
         }
         if(deflt){
             return deflt;
